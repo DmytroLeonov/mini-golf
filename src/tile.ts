c7d627ba2e62364d2c8c1c2673797259e3b309f9
DmytroLeonov <leonovdmitriy02@gmail.com>
2025-03-03 00:38:49 +0200

    feat: render center dots

--- src/tile.ts
+++ src/tile.ts
@@ -5,12 +5,19 @@ export const tileTypes = ["fairway", "sand", "rough", "water", "tree"] as const;
 
 export type TileType = (typeof tileTypes)[number];
 
-const tileColorMap: Record<TileType, string> = {
-  fairway: "lime",
-  rough: "white",
-  sand: "yellow",
-  tree: "green",
-  water: "blue",
+export type Color = {
+  dot: string;
+  bg: string;
+};
+
+export type TileColor = Record<TileType, Color>;
+
+const tileColorMap: TileColor = {
+  fairway: { bg: "lime", dot: "gray" },
+  rough: { bg: "white", dot: "lime" },
+  sand: { bg: "yellow", dot: "orange" },
+  tree: { bg: "green", dot: "green" },
+  water: { bg: "lightblue", dot: "blue" },
 };
 
 export interface ITile {
@@ -27,12 +34,24 @@ export class Tile implements ITile {
     } = state;
     const color = tileColorMap[this.type];
 
-    ctx.fillStyle = color;
+    ctx.fillStyle = color.bg;
     ctx.fillRect(
       this.pos.x * tileSize,
       this.pos.y * tileSize,
       tileSize,
       tileSize
     );
+
+    ctx.beginPath()
+    ctx.moveTo(this.pos.x, this.pos.y)
+    ctx.arc(
+      this.pos.x * tileSize + tileSize / 2,
+      this.pos.y * tileSize + tileSize / 2,
+      tileSize * 0.1,
+      0,
+      2 * Math.PI
+    );
+    ctx.fillStyle = color.dot;
+    ctx.fill();
   }
 }
