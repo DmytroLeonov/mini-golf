e90431288f1f6267ac92669d4b6028199edd68ea
DmytroLeonov <leonovdmitriy02@gmail.com>
2025-03-14 20:05:49 +0200

    feat: update circle render

--- src/render.ts
+++ src/render.ts
@@ -1,12 +1,20 @@
 import { assert } from "./assert";
 import { State } from "./state";
+import { Coord } from "./types";
+
+type CircleProportionalConfig = {
+  color: string;
+  radiusRatio: number;
+  strokeRatio?: number;
+  strokeColor?: string;
+};
 
 const seedSpan = document.querySelector<HTMLSpanElement>("#seed")!;
 assert(!!seedSpan, "seed span not found");
 const widthSpan = document.querySelector<HTMLSpanElement>("#width")!;
-assert(!!widthSpan, "seed span not found");
+assert(!!widthSpan, "width span not found");
 const heightSpan = document.querySelector<HTMLSpanElement>("#height")!;
-assert(!!heightSpan, "seed span not found");
+assert(!!heightSpan, "height span not found");
 
 export function setText(state: State): void {
   const {
@@ -19,58 +27,100 @@ export function setText(state: State): void {
 }
 
 function renderBall(state: State): void {
-  const {
-    ball,
-    config: { tileSize },
-    ctx,
-  } = state;
+  const { ball } = state;
 
-  ctx.beginPath();
-  ctx.arc(
-    ball.x * tileSize + tileSize / 2,
-    ball.y * tileSize + tileSize / 2,
-    tileSize * 0.3,
-    0,
-    2 * Math.PI
-  );
-  ctx.fillStyle = "white";
-  ctx.fill();
-  ctx.lineWidth = tileSize * 0.1;
-  ctx.strokeStyle = "black";
-  ctx.stroke();
+  renderCircle(state, ball, {
+    color: "white",
+    radiusRatio: 0.3,
+    strokeColor: "black",
+    strokeRatio: 0.1,
+  });
 }
 
 function renderHole(state: State): void {
   const {
     level: { hole },
-    config: { tileSize },
-    ctx,
   } = state;
 
-  ctx.beginPath();
-  ctx.arc(
-    hole.x * tileSize + tileSize / 2,
-    hole.y * tileSize + tileSize / 2,
-    tileSize * 0.4,
-    0,
-    2 * Math.PI
-  );
-  ctx.fillStyle = "black";
-  ctx.fill();
+  renderCircle(state, hole, { color: "black", radiusRatio: 0.4 });
 }
 
-export function render(state: State): void {
-  const { level, canvas, ctx } = state;
-
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  for (const row of level.field) {
+function renderTiles(state: State): void {
+  for (const row of state.level.field) {
     for (const tile of row) {
       tile.render(state);
     }
   }
+}
 
+function renderCircle(
+  state: State,
+  pos: Coord,
+  config: CircleProportionalConfig
+): void {
+  const {
+    ctx,
+    config: { tileSize },
+  } = state;
+  const { color, radiusRatio, strokeRatio, strokeColor } = config;
+
+  if (!!strokeRatio !== !!strokeColor) {
+    throw new Error("either supply both strokeColor and strokeRatio or none");
+  }
+
+  ctx.beginPath();
+  ctx.arc(
+    pos.x * tileSize + tileSize / 2,
+    pos.y * tileSize + tileSize / 2,
+    tileSize * radiusRatio,
+    0,
+    2 * Math.PI
+  );
+  ctx.fillStyle = color;
+  ctx.fill();
+
+  if (strokeRatio && strokeColor) {
+    ctx.lineWidth = tileSize * strokeRatio;
+    ctx.strokeStyle = strokeColor;
+    ctx.stroke();
+  }
+}
+
+function renderDot(state: State, pos: Coord): void {
+  renderCircle(state, pos, { color: "red", radiusRatio: 0.1 });
+}
+
+const offsets = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+];
+
+function renderPossibleMoves(state: State): void {
+  const ballPos = state.ball;
+
+  for (const [x, y] of offsets) {
+    const dotPos: Coord = {
+      x: ballPos.x + x,
+      y: ballPos.y + y,
+    };
+    renderDot(state, dotPos);
+  }
+}
+
+export function render(state: State): void {
+  const { canvas, ctx } = state;
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  renderTiles(state);
   renderBall(state);
   renderHole(state);
+  renderPossibleMoves(state);
 }
